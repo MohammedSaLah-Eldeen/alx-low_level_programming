94844446a2a85e9d673d2b7a0bd4d295d0d2db75
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-06-16 14:21:29 +0000

    task 07

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - runs code inside it.
+ * Return: indication of successful output.
+ */
+int main(void)
+{
+char z;
+for (z = 'z'; z >= 'a'; z--)
+{
+putchar(z);
+}
+putchar('\n');
+return (0);
+}
