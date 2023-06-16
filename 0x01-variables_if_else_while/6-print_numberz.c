32d408567d98fb624aa4b9836cb3e80aaa4f2f25
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-06-16 14:17:47 +0000

    task 06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - runs code inside it.
+ * Return: indication of successful output.
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
