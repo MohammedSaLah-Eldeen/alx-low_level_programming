f85056a813c16f237f2492ae9be6cd236c8b2cc2
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-06-16 14:06:05 +0000

    task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - runs code inside it.
+ * Return: indication of successful output.
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
