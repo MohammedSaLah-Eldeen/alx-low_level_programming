d548a025859e412328882f62cdc9500a529551bc
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-06-26 13:40:27 +0000

    task 05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*rev_string - function that reverses a string.
+*@s: table of caracter
+*Return: Void
+*/
+void rev_string(char *s)
+{
+char* ps = s;
+
+int largest_index = strlen(s) - 1;
+int i = largest_index;
+
+while (i >= ((largest_index + 1) / 2))
+{
+char vessel = *(s + i);
+*(s + i) = *(ps + largest_index - i);
+*(ps + largest_index - i) = vessel;
+i--;
+}
+}
