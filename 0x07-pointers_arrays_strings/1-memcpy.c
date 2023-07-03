c82b3fcbaa0eca65d550241b932c1e22d57986dc
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-07-03 10:51:10 +0000

    task 01

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+***_memcpy - function that fills memory with a constant byte.
+*@dest: poiter of char type
+*@src: integer
+*@n: unsigned int n
+*Return: @s char value
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+
+*(dest + i) = *(src + i);
+
+}
+return (dest);
+}
