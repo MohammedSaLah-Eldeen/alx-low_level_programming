e898c2df9402f0c8d5290689f321f5f5740e6ccb
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-07-07 18:29:17 +0000

    task 03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,23 @@
 int main(int argc, char *argv[])
 {
 int n1, n2;
+char *endptr1;
+char *endptr2;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-else
+
+n1 = strtol(argv[1], &endptr1, 10);
+n2 = strtol(argv[2], &endptr2, 10);
+if (*endptr1 != '\0' || *endptr2 != '\0')
 {
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("Error\n");
+return (1);
 }
+printf("%d\n", n1 * n2);
+
 
 return (0);
 }
