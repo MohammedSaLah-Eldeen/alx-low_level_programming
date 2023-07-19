5e573e0fc9b80da8dc15635ca44c85a1c2ef3169
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-07-19 09:40:28 +0000

    tasks 1,  2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+* int_index - ...
+* @array: ...
+* @size: ...
+* @cmp: ...
+*
+* Return: ...
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+for (; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+}
+}
+
+return (-1);
+}
