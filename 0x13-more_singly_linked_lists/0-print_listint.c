fe18ec479a615a92cbd532292439e775354109ec
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-08-01 10:14:04 +0000

    task 00

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+size_t nodes = 0;
+const list_t *plist = h;
 
-while (h)
+while (plist != NULL)
 {
 printf("%d\n", h->n);
-num++;
-h = h->next;
+nodes++;
+plist = plist->next;
 }
-
-return (num);
+return (nodes);
 }
