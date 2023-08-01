51b74871e7ccf4251e87369a194778848e745df4
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-08-01 14:32:00 +0000

    task 05 modification

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,10 @@ void free_listint2(listint_t **head)
 {
 listint_t *next, *current;
 
+
+if (head == NULL || *head == NULL)
+return;
+
 current = *head;
 while (current != NULL)
 {
@@ -19,5 +23,4 @@ free(current);
 current = next;
 }
 *head = NULL;
-head = NULL;
 }
