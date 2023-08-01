5bb62066bd13169e4b81dc98ae7da1caa2f4185b
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-08-01 14:48:44 +0000

    task 00 modification

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,10 +8,10 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
+size_t nodes = 0;
 
 if (h == NULL)
-return (0);
+return (nodes);
 
 while (h != NULL)
 {
@@ -20,6 +20,6 @@ h = h->next;
 count++;
 }
 
-return (count);
+return (nodes);
 
 }
