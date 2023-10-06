b92571d86de351c94efc80f829807e0b75156b41
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-10-06 14:15:31 +0000

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: table to retrieve value from
+ * @key: key to find value
+ *
+ * Return: value associated with key, or NULL if key cannot be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  unsigned long int index;
+  hash_node_t traverser;
+
+  if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+      key == NULL || strlen(key) == 0)
+   return (NULL);
+
+  index = key_index((const unsigned char *)key, ht->size);
+  traverser = ht->array[index];
+  while (traveser != NULL)
+    {
+      if (strcmp(traverser->key, key) == 0)
+	return (traverser->value);
+
+      traverser = traverser->next;
+    }
+  return (NULL);
+}
